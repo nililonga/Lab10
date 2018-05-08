fd239e44d2bf68984c6eabf79ffb642ba7e13f71
noelle none <noe.ililonga@gmail.com>
2018-05-07 21:51:11 -0400

    first commit

---
+++ Lab10CircleClass/Circle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10CircleClass
+{
+    class Circle
+
+    {
+        private string FormatNumber (double x);
+
+        private Member radius;
+
+        public double Radius { get; set; }
+
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            double CalCirc = (2 * Math.PI * Radius);
+            return Radius;
+        }
+        public string CalculateFormattedArea()
+        {
+            string CFA = Convert.ToString(2 * Math.PI * Radius);
+            return CFA;
+        }
+        public string CalculateArea()
+        {
+            string CFA = Convert.ToString(2 * Math.PI * Radius);
+            return CFA;
+        }
+        public double CalculateFormattedCircumference()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        private string FormatNumber (double x)
+        {
+
+        }
+    }
+
+    internal class Member
+    {
+    }
+}
+       
+        
+
+
+
+
+
+
+
+
+        
+
+
+
+
+    
+
---
+++ Lab10CircleClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab10CircleClass
+{
+    class Program
+    {
+        public static bool True { get; private set; }
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                float Value;
+                string Answer;
+
+                Console.Write($"enter the {radius}:  ");
+                Value = (Convert.ToInt32(Console.ReadLine()));
+                int int.MaxValue = Convert.ToInt32(Value);
+                //bool ValidRadius = Regex.IsMatch(Value, "\\d{1,5000}.\\d{1,5000}");
+                //Console.WriteLine(ValidRadius);
+
+                if ()
+                {
+                    //call class
+
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid entry");
+                }
+
+                //prompt user to continue
+                Console.WriteLine("Would you like to continue? (Y/N)");
+                Answer = (Console.ReadLine());
+
+                string Answer;
+                while (Answer == "Y") ;
+            }
+        }
+        
+    }
+}
